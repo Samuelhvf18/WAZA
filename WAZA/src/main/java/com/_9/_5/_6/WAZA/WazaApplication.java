948af889fc34db0efeb1e49516e7d343f0fbabaf
Samuel Villca <villcafloressamuelhenry@gmail.com>
2026-05-29 14:03:09 -0400

    Primer Commit

---
+++ WAZA/src/main/java/com/_9/_5/_6/WAZA/WazaApplication.java
@@ -0,0 +1,13 @@
+package com._9._5._6.WAZA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WazaApplication.class, args);
+	}
+
+}
---
+++ WAZA/src/test/java/com/_9/_5/_6/WAZA/WazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com._9._5._6.WAZA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
